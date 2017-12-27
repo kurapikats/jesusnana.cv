ac87455500645495a3f8fa37608c9f726f0cb7cf
kurapikats <kurapikats@yahoo.com>
2017-12-28 07:16:43 +0800

    add accounts component

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,1 +1,7 @@
+import { Accounts } from 'meteor/accounts-base'
+
 import './routes.jsx'
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
+})
--- imports/ui/layouts/main-layout.jsx
+++ imports/ui/layouts/main-layout.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import AccountsUIWrapper from '../components/common/AccountsUIWrapper.jsx'
 
 class MainLayout extends Component {
   render() {
     return (
       <div className="main-layout">
+        <AccountsUIWrapper />
         <a href="/">Home</a><br />
         <a href="/about">About</a>
         <br />

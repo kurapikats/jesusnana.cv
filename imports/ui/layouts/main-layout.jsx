95eec94c811ab744d9626cc867bfbacb9afb8fd8
kurapikats <kurapikats@yahoo.com>
2017-12-29 12:29:27 +0800

    update main layout

--- imports/ui/layouts/main-layout.jsx
+++ imports/ui/layouts/main-layout.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import AccountsUIWrapper from '../components/common/AccountsUIWrapper.jsx'
+import NavBar from '../components/navbar/NavBar.jsx'
+import SideNav from '../components/sidenav/SideNav.jsx'
 
 class MainLayout extends Component {
+  componentDidMount() {
+    $('.button-sidenav').sideNav({
+      edge: 'right',
+      closeOnClick: true,
+      draggable: true,
+    })
+  }
+
   render() {
     return (
       <div className="main-layout">
-        <AccountsUIWrapper />
-        <a href="/">Home</a><br />
-        <a href="/about">About</a>
-        <br />
+        <NavBar />
+        <SideNav />
         {this.props.contents}
       </div>
     )
